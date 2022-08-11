0fd51d4c170e66e3b1c4f96f6c0310a14572c94d
Cassiano Flores <cassiano.flores2424@gmail.com>
2022-08-11 10:33:26 -0300

    first-commit

---
+++ aula1/Aula1.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* Faca um programa em C que leia do teclado um vetor de 5 elementos (int) e apresente a media dos elementos impares */
+
+// sizeof(vet) -> determina o tamanho de um array em memória (ex: vet[5] é int, int tem 4 bytes, vet tem comprimento de 5, 5*4 = 20 é o tamanho em memória)
+// sizeof(vet)/sizeof(vet[0]) - > determina o comprimento de um array, divide o tamanho em memória pelo tamanho de 1 índice, retorna quantidade de elementos que suporta no array (lenght)
+
+int main () {
+
+    int vet[5];
+    int count = 0;
+    float media = 0;
+    float result;
+
+    for (int i=0; i < 5; i++){
+
+        printf("Digite o elemento do indice %d:  ", i+1);
+        scanf("%d", &vet[i]);
+
+        if (vet[i] % 2){    //if (vet[i] % 2 != 0)  eh desnecessario, uma operacao a mais
+
+            media = media + vet[i];
+            count++;
+        }
+    }
+
+    //putchar('\n');  -> imprime 1 caracter so , poderia ser printf("\n");
+
+    result = media / count;
+    printf("\nA media dos numeros impares do vetor eh: %f", result);  //%f ->  %5f ou %-5f  imprime com MINIMO de casas decimais
+
+    return 0;
+}
---
+++ aula2/Aula2.c
@@ -0,0 +1,140 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/* 
+    Fazer um programa para jogar par ou impar com o PC
+        -->  Ganha o jogo quem vencer 3 partidas consecutivas (empate nao conta)
+*/
+
+int main() {
+
+    char choosePlayer;
+    int numberPlayer;
+    char choosePC;
+    int numberPC;
+    char winners[3];
+
+    srand(time(NULL));  //randomizar
+
+        for (int i = 0; i < 3; i++){  //até 3 vitorias consecutivas
+            
+            printf("\nVoce escolhe par ou impar? (p/i): ");  //par/impar do player
+            scanf("%s", &choosePlayer);
+
+            printf("\nDigite o numero: ");  //numero do player
+            scanf("%d", &numberPlayer);
+
+            numberPC = rand();   //numero do pc
+            if ((numberPC % 2) == 0){     //par/impar do pc
+
+                choosePC = 'p';
+            }
+            else{
+                choosePC = 'i';
+            }
+
+            //------------------------------------------------------------------------------
+
+            if (choosePlayer == choosePC){   //se for empate, zera tudo e começa de novo
+                printf("\nEMPATE!\n");
+                i = -1;
+            }
+            else{
+
+                if (((numberPlayer + numberPC) % 2) == 0){   //deu par
+                    printf("\nDEU PAR!\n");
+
+                    if (choosePlayer == 'p'){    //jogador escolheu par
+
+                        if (i == 0){
+
+                            winners[i] = 'j';
+                        }
+                        else{
+
+                            if (winners[i-1] == 'j'){  //ultima rodada o jogador ganhou
+
+                                winners[i] = 'j';
+                            }
+                            else{
+
+                                winners[0] = 'j';
+                                i = 0;               //se nao tinha sido o jogador, reseta a sequencia
+                            }
+                        }
+                    }
+                    else{                //maquina escolheu par
+
+                        if (i == 0){
+
+                            winners[i] = 'm';
+                        }
+                        else{
+
+                            if (winners[i-1] == 'm'){  //ultima rodada a maquina ganhou
+
+                                winners[i] = 'm';
+                            }
+                            else{
+
+                                winners[0] = 'm';
+                                i = 0;               //se nao tinha sido a maquina, reseta a sequencia
+                            }
+                        }
+                    }
+                }
+                else{    //deu impar
+                    printf("\nDEU IMPAR!\n");
+
+                    if (choosePlayer == 'i'){    //jogador escolheu impar
+
+                        if (i == 0){
+
+                            winners[i] = 'j';
+                        }
+                        else{
+
+                            if (winners[i-1] == 'j'){  //ultima rodada o jogador ganhou
+
+                                winners[i] = 'j';
+                            }
+                            else{
+
+                                winners[0] = 'j';
+                                i = 0;               //se nao tinha sido o jogador, reseta a sequencia
+                            }
+                        }
+                    }
+                    else{                //maquina escolheu impar
+
+                        if (i == 0){
+
+                            winners[i] = 'm';
+                        }
+                        else{
+
+                            if (winners[i-1] == 'm'){  //ultima rodada a maquina ganhou
+
+                                winners[i] = 'm';
+                            }
+                            else{
+
+                                winners[0] = 'm';
+                                i = 0;               //se nao tinha sido a maquina, reseta a sequencia
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        if (winners[0] == 'j'){
+            printf("\nJOGADOR VENCEU!");
+        }
+        else{
+            printf("\nMAQUINA VENCEU!");
+        }
+
+    return 0;
+}
