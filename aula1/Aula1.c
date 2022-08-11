debb5e4333ecb613b28add78a860e1c40df0f1e8
cassiano-flores <cassiano.flores2424@gmail.com>
2022-08-11 18:28:30 -0300

    average of odd

--- aula1/Aula1.c
+++ aula1/Aula1.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-/* Faca um programa em C que leia do teclado um vetor de 5 elementos (int) e apresente a media dos elementos impares */
+/* Faca um programa em C que leia do teclado um vetor de 5 elementos (int) e apresente a media dos elementos impares apenas */
 
 // sizeof(vet) -> determina o tamanho de um array em memória (ex: vet[5] é int, int tem 4 bytes, vet tem comprimento de 5, 5*4 = 20 é o tamanho em memória)
 // sizeof(vet)/sizeof(vet[0]) - > determina o comprimento de um array, divide o tamanho em memória pelo tamanho de 1 índice, retorna quantidade de elementos que suporta no array (lenght)
