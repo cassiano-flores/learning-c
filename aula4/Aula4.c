eaad8614f3a662736f0967caf6751f1fdd2b4060
cassiano-flores <cassiano.flores2424@gmail.com>
2022-08-18 18:56:56 -0300

    aula4

--- aula4/Aula4.c
+++ aula4/Aula4.c
@@ -5,13 +5,44 @@
 
 int main(){
 
+    char str[1024];
 
+    printf("Digita uma frase: ");
+    gets(str);
 
+    //convertToBin(str);   usar a funcao
+
+    int ind = 0;
+
+    do{
+        printBin(str[ind++]);
+        putchar(' ');
+        if( !(ind % 10) )  putchar('\n');
+
+    }while(str[ind] != '\0');
+
+    
     return 0;
 }
 
+void printBin(unsigned char x){
 
-void convertToBin(char frase[]){
+    unsigned char mask = 0x80;
+    int k;
+
+    for(k = 0; k < 8; k++){
+
+        (x & mask) ? putchar('1') : putchar('0');
+        mask = mask >> 1;
+    }
+}
 
 
+void convertToBin(char str[1024]){  //por funcao, sem estar na main
+
+    for (int i = 0; str[i] != '\0'; i++){
+
+        printBin(str[i]);
+        putchar('\n');
+    }
 }
