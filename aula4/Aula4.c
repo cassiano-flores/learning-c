850320328532e6f19b481fb8c97115e0beb46614
Cassiano Flores <cassiano.flores2424@gmail.com>
2022-08-18 15:28:09 -0300

    aula4

---
+++ aula4/Aula4.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+//Faça um programa que leia uma frase do teclado e imprima em binário
+
+int main(){
+
+
+
+    return 0;
+}
+
+
+void convertToBin(char frase[]){
+
+
+}
